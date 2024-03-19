ecea6b28fb5fd2262a0eb5fce55dee260c5c466c
amankayat <amankayat789@gmail.com>
2024-03-19 10:01:27 +0530

    updated

--- login-service/src/main/java/com/blueyonder/loginservice/service/UserServiceImpl.java
+++ login-service/src/main/java/com/blueyonder/loginservice/service/UserServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.blueyonder.loginservice.controller.UserController;
+
 import com.blueyonder.loginservice.entities.UserEntity;
 import com.blueyonder.loginservice.exceptions.PasswordMismatchException;
 import com.blueyonder.loginservice.exceptions.UserNameNotFoundException;
